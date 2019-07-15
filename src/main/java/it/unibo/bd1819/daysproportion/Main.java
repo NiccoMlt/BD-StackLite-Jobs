1340d2d8d3456066f1e5432f731fa4d38f9d22bc
Niccolò Maltoni <nicco.mlt@gmail.com>
2019-07-15 12:07:46 +0200

    Refactor repo

--- src/main/java/it/unibo/bd1819/daysproportion/Main.java
+++ src/main/java/it/unibo/bd1819/daysproportion/Main.java
@@ -1,4 +1,4 @@
-package it.unibo.bd1819;
+package it.unibo.bd1819.daysproportion;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -7,18 +7,25 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.mapreduce.Job;
 
 public class Main {
+    /**
+     * Launch the job.
+     *
+     * @param args command line args
+     *
+     * @throws Exception if something goes wrong
+     */
     public static void main(String[] args) throws Exception {
         final List<Job> jobs = new ArrayList<>();
         final Configuration conf = new Configuration();
 
-//        jobs.add(createDirectorsMovieJoin(conf));
+        // jobs.add(createDirectorsMovieJoin(conf));
 
-        for (final Job job: jobs) {
+        for (final Job job : jobs) {
             if (!job.waitForCompletion(true)) {
                 System.exit(1);
             }
         }
 
-//        createSortJob(conf).waitForCompletion(true);
+        // createSortJob(conf).waitForCompletion(true);
     }
 }
