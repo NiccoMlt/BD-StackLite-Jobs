58c4538a8f764a4d914b10cf27e5e41c8f0b4b5a
Niccolò Maltoni <nicco.mlt@gmail.com>
2019-07-22 11:01:34 +0200

    Add second main

---
+++ src/main/java/it/unibo/bd1819/scoreanswersbins/Main.java
@@ -0,0 +1,26 @@
+package it.unibo.bd1819.scoreanswersbins;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+public class Main extends Configured implements Tool {
+
+    /**
+     * Launch the job.
+     *
+     * @param args command line args
+     * @throws Exception if something goes wrong
+     */
+    public static void main(final String[] args) throws Exception {
+        int res = ToolRunner.run(new Configuration(), new Main(), args);
+        System.exit(res);
+    }
+    
+    @Override
+    public int run(final String[] args) throws Exception {
+        // TODO
+        return 0;
+    }
+}
