c6432f42ec81a47d72f485baee6195e763bf691e
Niccolò Maltoni <nicco.mlt@gmail.com>
2019-07-28 15:23:25 +0200

    working on sort

---
+++ src/main/java/it/unibo/bd1819/scoreanswersbins/sort/BinSortReducer.java
@@ -0,0 +1,7 @@
+package it.unibo.bd1819.scoreanswersbins.sort;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class BinSortReducer extends Reducer<Text, Text, Text, Text> {
+}
