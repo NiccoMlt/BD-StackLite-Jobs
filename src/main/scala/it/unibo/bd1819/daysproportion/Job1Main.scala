e1119218ef38b9c3ed1ea7689f76ed4b6ec38c7f
Luca Semprini <luca.semprini10@studio.unibo.it>
2019-09-03 12:24:24 +0200

    Updated documentation

--- src/main/scala/it/unibo/bd1819/daysproportion/Job1Main.scala
+++ src/main/scala/it/unibo/bd1819/daysproportion/Job1Main.scala
@@ -3,7 +3,6 @@ package it.unibo.bd1819.daysproportion
 import it.unibo.bd1819.common.{Configuration, DateUtils, JobMainAbstract}
 import org.apache.spark.SparkContext
 import org.apache.spark.sql.SQLContext
-import org.apache.spark.sql.functions._
 
 class Job1Main extends JobMainAbstract {
 
@@ -27,40 +26,22 @@ class Job1Main extends JobMainAbstract {
      */
     val dateAndTagDF = this.questionTagsDF.join(onlyDateDF, "Id").drop("Id")
     dateAndTagDF.createOrReplaceTempView("dateAndTagDF")
-    dateAndTagDF.cache()
-    
-    /* Create a Sequence that represents the current columns names */
-    // val columnNamesToSelect = Seq("tag", "IsWorkDay")
-    
-    /* Add to the DF a column that represents how many questions with a specific Tag appear
-     * in the Data Frame.
-     
-    val countDF = dateAndTagDF
-      .select(columnNamesToSelect.map(c => col(c)): _*)
-      .groupBy("tag")
-      .agg(count("IsWorkDay")
-        .as("Count"))
-    countDF.cache()
-    */
     
     /* Create a DF that has, associated to every tag, the proportion between how many
      * questions with that tag have been posted on workdays and how many on holidays.
      * Proportion float number will only have 2 decimals.
+     * This DF will also have a column that represents how many questions with a specific Tag appear
+     * in the Data Frame.
      */
-    val workHolyDF = sqlContext.sql("select tag, (round(" +
+    val finalDF = sqlContext.sql("select tag, (round(" +
       "(cast(sum(case when IsWorkDay = true then 1 else 0 end) as float)) / " +
       "(cast(sum(case when IsWorkDay = false then 1 else 0 end) as float)), " +
       "2)) as Proportion, " +
       "count(*) as Count " +
       "from dateAndTagDF group by tag")
     
-    /* The final DF will be a join between the previous one and the DF withe count information
-     * (the amount of questions posted for every tag).
-     */
-    // val finalJoinDF = workHolyDF.join(countDF, "tag")
-    
     /* Show the first 20 rows for this DF */
-    workHolyDF.show()
+    finalDF.show()
   }
 }
 
