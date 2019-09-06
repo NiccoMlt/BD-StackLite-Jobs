16dbd1c76e3ea59d34ec1da31671717e6bbc1d3d
Luca Semprini <luca.semprini10@studio.unibo.it>
2019-09-06 12:21:54 +0200

    Improved bin thresholds

--- src/main/scala/it/unibo/bd1819/scoreanswersbins/Job2Main.scala
+++ src/main/scala/it/unibo/bd1819/scoreanswersbins/Job2Main.scala
@@ -15,7 +15,8 @@ class Job2Main extends JobMainAbstract {
     scoreAnswersDF.cache()
     
     /* Join the previously obtained DF to the question_tags DF, dropping the useless column containing the Ids.
-     * Select then all columns from the resulting DF, and map the Score and AnswerCount columns into one Bin
+     * Select then all columns from the resulting DF, filter "NA" values that will affect the analysis
+     * and finally map the Score and AnswerCount columns into one Bin
      * column that will have data representing in which Bin
      * (Between HIGH_SCORE_HIGH_COUNT, HIGH_SCORE_LOW_COUNT, LOW_SCORE_HIGH_COUNT, LOW_SCORE_LOW_COUNT) the pair
      * (Score, AnswerCount) must be located.
@@ -23,12 +24,11 @@ class Job2Main extends JobMainAbstract {
      */
     val binDF = questionTagsDF.join(scoreAnswersDF, "Id").drop("Id")
       .select("tag", "Score", "AnswerCount")
+      .filter($"Score" notEqual "NA")
+      .filter($"AnswerCount" notEqual "NA")
       .map(row => (row.getString(0),
-        Bin.getBinFor(
-        Integer.parseInt(if (row.getString(1) == "null" || row.getString(1) == "NA") "0" else row.getString(1)),
-          Job2Main.IMPROVED_SCORE_THRESHOLD,
-          Integer.parseInt(if (row.getString(2) == "null" || row.getString(2) == "NA") "0" else row.getString(2)),
-          Job2Main.IMPROVED_ANSWER_THRESHOLD).toString))
+        Bin.getBinFor(Integer.parseInt(row.getString(1)), Job2Main.IMPROVED_SCORE_THRESHOLD,
+          Integer.parseInt(row.getString(2)), Job2Main.IMPROVED_ANSWER_THRESHOLD).toString))
       .withColumnRenamed("_1", "Tag")
       .withColumnRenamed("_2", "Bin")
     binDF.createOrReplaceTempView("binDF")
@@ -54,8 +54,13 @@ object Job2Main {
    * Improved thresholds for Score and AnswerCount, modified due to multiple trials.
    * These values should balance enough the dataset into the four bins.
    */
-  private val IMPROVED_SCORE_THRESHOLD = 200
-  private val IMPROVED_ANSWER_THRESHOLD = 50
+  private val IMPROVED_SCORE_THRESHOLD = 10
+  private val IMPROVED_ANSWER_THRESHOLD = 5
+  
+  /*
+  * Circa 1/3 delle domande hanno 0 risposte.
+  * Circa 2/5 delle domande hanno meno di 4 come score.
+  */
   
   def apply(): Job2Main = new Job2Main()
 }
